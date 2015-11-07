62d43b9131d6d062f290853a32acb37a05da3755
Geovanni Barreto <geovanni_barreto@hotmail.com>
2015-11-07 20:24:46 -0200

    cadastro de fabrica

---
+++ TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/Controllers/FabricaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TarifacaoEnergiaEletrica.Models;
+
+namespace TarifacaoEnergiaEletrica.Controllers
+{
+    public class FabricaController : Controller
+    {
+        // GET: Fabrica
+        public ActionResult CadastroFabrica()
+        {
+            var fabrica = new Fabrica();
+            return View(fabrica);
+        }
+
+        [HttpPost]
+        public ActionResult CadastroFabrica(Fabrica fabrica)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Resultado", fabrica);
+            }
+            return View(fabrica);
+        }
+        
+        public ActionResult Resultado(Fabrica fabrica)
+        {
+            return View(fabrica);
+        }
+    }
+}
--- TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/Models/Fabrica.cs
+++ TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/Models/Fabrica.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace TarifacaoEnergiaEletrica.Models
 {
-    class Fabrica
+    public partial class Fabrica
     {
         public int IdFabrica { get; set; }
         public int IdCliente { get; set; }
         public String CNPJ { get; set; }
+        [Required(ErrorMessage = "Informe CNPJ")]
         public String Endereco { get; set; }
+        //[Required(ErrorMessage = "Informe endereco")]
     }
 }
