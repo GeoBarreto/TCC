55ff834a145b40d84754cc0d9435d3865747e66d
Geovanni Barreto <geovanni_barreto@hotmail.com>
2015-11-07 21:07:51 -0200

    Revert "login"
    This reverts commit e232c71c9767deacd5019fbb61840d63bce17f22.

--- TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/App_Start/Startup.Auth.cs
+++ TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/App_Start/Startup.Auth.cs
@@ -25,7 +25,7 @@ namespace TarifacaoEnergiaEletrica
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Home/Login"),
+                LoginPath = new PathString("/Account/Login"),
                 Provider = new CookieAuthenticationProvider
                 {
                     // Enables the application to validate the security stamp when the user logs in.
