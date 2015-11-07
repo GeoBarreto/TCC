ee8565b9f90cd7ebac7b9ee8cccf0306950be6c3
Geovanni Barreto <geovanni_barreto@hotmail.com>
2015-11-07 21:08:28 -0200

    Revert "cadastro de fabrica"
    This reverts commit 62d43b9131d6d062f290853a32acb37a05da3755.

--- TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/App_Start/RouteConfig.cs
+++ TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TarifacaoEnergiaEletrica
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Fabrica", action = "CadastroFabrica", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/Models/Fabrica.cs
+++ TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/Models/Fabrica.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace TarifacaoEnergiaEletrica.Models
 {
-    public partial class Fabrica
+    class Fabrica
     {
         public int IdFabrica { get; set; }
         public int IdCliente { get; set; }
         public String CNPJ { get; set; }
-        [Required(ErrorMessage = "Informe CNPJ")]
         public String Endereco { get; set; }
-        //[Required(ErrorMessage = "Informe endereco")]
     }
 }
