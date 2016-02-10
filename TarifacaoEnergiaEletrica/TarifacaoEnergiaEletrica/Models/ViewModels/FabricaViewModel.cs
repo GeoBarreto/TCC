d5d928faf5f8a49203b21fb5ba43f1260c2db7c3
Geovanni Barreto <geovanni_barreto@hotmail.com>
2016-02-10 00:19:51 -0200

    Reorganização de classes DAO, ViewModel e objetos

--- TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/Models/ViewModels/FabricaViewModel.cs
+++ TarifacaoEnergiaEletrica/TarifacaoEnergiaEletrica/Models/ViewModels/FabricaViewModel.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
 
-namespace TarifacaoEnergiaEletrica.Models
+namespace TarifacaoEnergiaEletrica.Models.ViewModels
 {
-    public partial class Fabrica
+    public partial class FabricaViewModel
     {
+        [Key]
         public int IdFabrica { get; set; }
         public int IdCliente { get; set; }
+        [Display(Name = "CNPF")]
+        [DataType(DataType.Text)]
         public String CNPJ { get; set; }
+        [Display(Name = "Endereço")]
+        [DataType(DataType.Text)]
         public String Endereco { get; set; }
         public int IdDistribuidora { get; set; }
+        [Display(Name = "Distribuidora")]
+        [DataType(DataType.Text)]
         public String NomeDistribuidora { get; set; }
     }
 }
